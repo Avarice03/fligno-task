2ddd6e4f28c505c786fe058e9784843eba9c42b9
Niel <dmendegorin@gmail.com>
2023-06-22 16:28:34 +0800

    redeploy

--- fligno-recipeez/src/components/services/RecipesService.js
+++ fligno-recipeez/src/components/services/RecipesService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://flignorecipeez-api.onrender.com";
+const BASE_URL = "https://flignorecipeez-api.onrender.com/api/v1";
 // const BASE_URL = "http://localhost:3069/api/v1";
 
 export const getUserDetails = async () => {
